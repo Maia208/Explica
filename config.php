a04f7487190488b9f5588570fb5867fa53714ce2
Andre Alves <andremaialves@gmail.com>
2025-05-07 09:47:27 +0100

    config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER['SERVER_NAME']=="localhost") {
+	$bd_host="localhost";
+	$bd_user="root";
+	$bd_password="";
+	$bd_database="exemplo2";
+}
+else {
+	$bd_host="localhost";
+	$bd_user="usr21";
+	$bd_password="dacic2020";
+	$bd_database="usr21";
+}
+?>
